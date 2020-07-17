01dd3cc7813cea2392a18a826b98563bfee68c13
Andrew Codispoti <andrewcod749@gmail.com>
2020-07-16 23:27:17 -0700

    Basic log parsing and extraction of information.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+//! Structure log files to something that can be understood and parsed 
+//!
+ 
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Args {
+    #[structopt(name = "FILE", parse(from_os_str))]
+    /// Files to process
+    file: Vec<PathBuf>,
+}
+
+fn main() {
+    let args: Args = Args::from_args();
+    println!("Arguments provided: {:?}", args)
+}
